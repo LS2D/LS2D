0d657c0f103a642fbf88ef0f29fdd33d351ae691
julietbravo <bart@vanstratum.com>
2019-03-18 20:11:18 +0100

    Modified MicroHH LS2D script for 2.0 (but still the old ASCII) input

--- models/MicroHH/LS2D.py
+++ models/MicroHH/LS2D.py
@@ -31,6 +31,8 @@ if (__name__ == '__main__'):
         'write_log'   : True
         }
 
+    microhh_version = 1     # {1,2}
+
     header('Creating LES input')
 
     # Download the ERA5 data (or check whether it is available local)
@@ -44,7 +46,7 @@ if (__name__ == '__main__'):
 
     nl   = mht.Read_namelist()
     grid = mht.Stretched_grid(nl['grid']['ktot'], 130, 30, 20, 150)     # 128 hrv grid
-    grid.plot()
+    #grid.plot()
 
     # Create nudge factor, controlling where nudging is aplied, and time scale
     tau_nudge = 7200         # Nudge time scale (s)
@@ -95,24 +97,43 @@ if (__name__ == '__main__'):
     mht.write_output('testbed.prof', variables, grid.z.size)
 
     # 3. Time varying nudging profiles
-    mht.write_time_profs('thlnudge.timeprof', grid.z, e5.time_sec, thl)
-    mht.write_time_profs('qtnudge.timeprof',  grid.z, e5.time_sec, qt )
-    mht.write_time_profs('unudge.timeprof',   grid.z, e5.time_sec, u  )
-    mht.write_time_profs('vnudge.timeprof',   grid.z, e5.time_sec, v  )
+    # Switch between old file names 1.x, and new ones in 2.0
+    if microhh_version == 1:
+        names = {'thl_nudge': 'thlnudge.timeprof', 'qt_nudge': 'qtnudge.timeprof',
+                 'u_nudge': 'unudge.timeprof', 'v_nudge': 'vnudge.timeprof',
+                 'thl_ls': 'thlls.timeprof', 'qt_ls': 'qtls.timeprof',
+                 'u_ls': 'uls.timeprof', 'v_ls': 'vls.timeprof',
+                 'ug': 'ug.timeprof', 'vg': 'vg.timeprof', 'wls': 'wls.timeprof'}
+
+    elif microhh_version == 2:
+        names = {'thl_nudge': 'thl_nudge.time', 'qt_nudge': 'qt_nudge.time',
+                 'u_nudge': 'u_nudge.time', 'v_nudge': 'v_nudge.time',
+                 'thl_ls': 'thl_ls.time', 'qt_ls': 'qt_ls.time',
+                 'u_ls': 'u_ls.time', 'v_ls': 'v_ls.time',
+                 'ug': 'u_geo.time', 'vg': 'v_geo.time', 'wls': 'w_ls.time'}
+
+    mht.write_time_profs(names['thl_nudge'], grid.z, e5.time_sec, thl)
+    mht.write_time_profs(names['qt_nudge'],  grid.z, e5.time_sec, qt )
+    mht.write_time_profs(names['u_nudge'],   grid.z, e5.time_sec, u  )
+    mht.write_time_profs(names['v_nudge'],   grid.z, e5.time_sec, v  )
 
     # 4. Time varying large scale advective tendencies
-    mht.write_time_profs('thlls.timeprof',    grid.z, e5.time_sec, thlls)
-    mht.write_time_profs('qtls.timeprof',     grid.z, e5.time_sec, qtls )
-    mht.write_time_profs('uls.timeprof',      grid.z, e5.time_sec, uls  )
-    mht.write_time_profs('vls.timeprof',      grid.z, e5.time_sec, vls  )
+    mht.write_time_profs(names['thl_ls'], grid.z, e5.time_sec, thlls)
+    mht.write_time_profs(names['qt_ls'],  grid.z, e5.time_sec, qtls )
+    mht.write_time_profs(names['u_ls'],   grid.z, e5.time_sec, uls  )
+    mht.write_time_profs(names['v_ls'],   grid.z, e5.time_sec, vls  )
 
-    # 5. Time varying geostrophic wind
-    mht.write_time_profs('ug.timeprof',       grid.z, e5.time_sec, ug)
-    mht.write_time_profs('vg.timeprof',       grid.z, e5.time_sec, vg)
-
-    # 6. Time varying subsidence
-    mht.write_time_profs('wls.timeprof',      grid.z, e5.time_sec, w)
+    # 5. Time varying geostrophic wind and subsidence
+    mht.write_time_profs(names['ug'],  grid.z, e5.time_sec, ug)
+    mht.write_time_profs(names['vg'],  grid.z, e5.time_sec, vg)
+    mht.write_time_profs(names['wls'], grid.z, e5.time_sec, w)
 
     # 7. Time varying surface variables
-    variables = {'t':e5.time_sec, 'sbot[thl]':e5.wth_mean, 'sbot[qt]':e5.wq_mean, 'pbot':e5.ps_mean}
-    mht.write_output('testbed.time', variables, e5.time_sec.size)
+    if microhh_version == 1:
+        variables = {'t': e5.time_sec, 'sbot[thl]': e5.wth_mean, 'sbot[qt]': e5.wq_mean, 'pbot': e5.ps_mean}
+        mht.write_output('testbed.time', variables, e5.time_sec.size)
+
+    elif microhh_version == 2:
+        mht.write_output('thl_sbot.time', {'time': e5.time_sec, 'thl_sbot': e5.wth_mean}, e5.time_sec.size)
+        mht.write_output('qt_sbot.time',  {'time': e5.time_sec, 'qt_sbot': e5.wq_mean}, e5.time_sec.size)
+        mht.write_output('p_sbot.time',   {'time': e5.time_sec, 'p_sbot': e5.ps_mean}, e5.time_sec.size)
