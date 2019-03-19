dc616d1c6605b14527d666bf12be4f6b2ad763bb
julietbravo <bart@vanstratum.com>
2019-03-19 19:19:47 +0100

    Added NetCDF output for new input reader MicroHH2.0

--- models/MicroHH/LS2D.py
+++ models/MicroHH/LS2D.py
@@ -1,5 +1,6 @@
 import datetime
 import numpy as np
+import netCDF4 as nc
 from scipy.special import erf
 import sys
 import os
@@ -10,7 +11,7 @@ sys.path.append('{}/../../src/'.format(os.path.dirname(os.path.abspath(__file__)
 # Import the LS2D specific scripts
 from download_ERA5 import download_ERA5
 from read_ERA5     import Read_ERA
-from messages      import header, message
+from messages      import header, message, error
 
 # Import MicroHH specific tools
 import microhh_tools as mht
@@ -24,14 +25,17 @@ if (__name__ == '__main__'):
         'area_size'   : 1,
         'case_name'   : 'cabauw',
         #'base_path'   : '/nobackup/users/stratum/ERA5/LS2D/',  # KNMI
-        #'base_path'   : '/Users/bart/meteo/data/ERA5/LS2D/',   # Macbook
-        'base_path'   : '/home/scratch1/meteo_data/LS2D/',      # Arch
+        'base_path'   : '/Users/bart/meteo/data/LS2D/',   # Macbook
+        #'base_path'   : '/home/scratch1/meteo_data/LS2D/',      # Arch
         'start_date'  : datetime.datetime(year=2018, month=8, day=11, hour=4),
         'end_date'    : datetime.datetime(year=2018, month=8, day=11, hour=20),
         'write_log'   : True
         }
 
-    microhh_version = 1     # {1,2}
+    microhh_version = 2     # {1,2}
+    use_netcdf = True       # Only for microhh_version=2
+    float_type  = 'f4'      # {f4,f8}
+
 
     header('Creating LES input')
 
@@ -46,7 +50,7 @@ if (__name__ == '__main__'):
 
     nl   = mht.Read_namelist()
     grid = mht.Stretched_grid(nl['grid']['ktot'], 130, 30, 20, 150)     # 128 hrv grid
-    #grid.plot()
+    grid.plot()
 
     # Create nudge factor, controlling where nudging is aplied, and time scale
     tau_nudge = 7200         # Nudge time scale (s)
@@ -92,48 +96,109 @@ if (__name__ == '__main__'):
     mht.replace_namelist_value('z0m',     e5.z0m_mean[0])
     mht.replace_namelist_value('z0h',     e5.z0h_mean[0])
 
-    # 2. Initial profiles
-    variables = {'z':grid.z, 'thl':thl[0,:], 'qt':qt[0,:], 'u':u[0,:], 'v':v[0,:], 'nudgefac':nudge_fac}
-    mht.write_output('testbed.prof', variables, grid.z.size)
+    if use_netcdf:
+        if microhh_version == 2:
 
-    # 3. Time varying nudging profiles
-    # Switch between old file names 1.x, and new ones in 2.0
-    if microhh_version == 1:
-        names = {'thl_nudge': 'thlnudge.timeprof', 'qt_nudge': 'qtnudge.timeprof',
-                 'u_nudge': 'unudge.timeprof', 'v_nudge': 'vnudge.timeprof',
-                 'thl_ls': 'thlls.timeprof', 'qt_ls': 'qtls.timeprof',
-                 'u_ls': 'uls.timeprof', 'v_ls': 'vls.timeprof',
-                 'ug': 'ug.timeprof', 'vg': 'vg.timeprof', 'wls': 'wls.timeprof'}
+            def add_nc_var(nc_file, name, dims, data):
+                var = nc_file.createVariable(name, float_type, dims)
+                var[:] = data[:]
 
-    elif microhh_version == 2:
-        names = {'thl_nudge': 'thl_nudge.time', 'qt_nudge': 'qt_nudge.time',
-                 'u_nudge': 'u_nudge.time', 'v_nudge': 'v_nudge.time',
-                 'thl_ls': 'thl_ls.time', 'qt_ls': 'qt_ls.time',
-                 'u_ls': 'u_ls.time', 'v_ls': 'v_ls.time',
-                 'ug': 'u_geo.time', 'vg': 'v_geo.time', 'wls': 'w_ls.time'}
+            nc_file = nc.Dataset('testbed_input.nc', mode='w', datamodel='NETCDF4')
 
-    mht.write_time_profs(names['thl_nudge'], grid.z, e5.time_sec, thl)
-    mht.write_time_profs(names['qt_nudge'],  grid.z, e5.time_sec, qt )
-    mht.write_time_profs(names['u_nudge'],   grid.z, e5.time_sec, u  )
-    mht.write_time_profs(names['v_nudge'],   grid.z, e5.time_sec, v  )
+            # Initial profiles
+            nc_file.createDimension('z', grid.kmax)
+            add_nc_var(nc_file, 'z', ('z'), grid.z)
 
-    # 4. Time varying large scale advective tendencies
-    mht.write_time_profs(names['thl_ls'], grid.z, e5.time_sec, thlls)
-    mht.write_time_profs(names['qt_ls'],  grid.z, e5.time_sec, qtls )
-    mht.write_time_profs(names['u_ls'],   grid.z, e5.time_sec, uls  )
-    mht.write_time_profs(names['v_ls'],   grid.z, e5.time_sec, vls  )
+            # Create a group called "init" for the initial profiles.
+            nc_group_init = nc_file.createGroup('init')
 
-    # 5. Time varying geostrophic wind and subsidence
-    mht.write_time_profs(names['ug'],  grid.z, e5.time_sec, ug)
-    mht.write_time_profs(names['vg'],  grid.z, e5.time_sec, vg)
-    mht.write_time_profs(names['wls'], grid.z, e5.time_sec, w)
+            add_nc_var(nc_group_init, 'thl', ('z'), thl[0,:])
+            add_nc_var(nc_group_init, 'qt',  ('z'), qt [0,:])
+            add_nc_var(nc_group_init, 'u',   ('z'), u  [0,:])
+            add_nc_var(nc_group_init, 'v',   ('z'), v  [0,:])
 
-    # 7. Time varying surface variables
-    if microhh_version == 1:
-        variables = {'t': e5.time_sec, 'sbot[thl]': e5.wth_mean, 'sbot[qt]': e5.wq_mean, 'pbot': e5.ps_mean}
-        mht.write_output('testbed.time', variables, e5.time_sec.size)
+            # Create a group called "timedep" for the timedep.
+            nc_group_timedep = nc_file.createGroup('timedep')
 
-    elif microhh_version == 2:
-        mht.write_output('thl_sbot.time', {'time': e5.time_sec, 'thl_sbot': e5.wth_mean}, e5.time_sec.size)
-        mht.write_output('qt_sbot.time',  {'time': e5.time_sec, 'qt_sbot': e5.wq_mean}, e5.time_sec.size)
-        mht.write_output('p_sbot.time',   {'time': e5.time_sec, 'p_sbot': e5.ps_mean}, e5.time_sec.size)
+            # Write the input times:
+            nc_group_timedep.createDimension('time_surface', e5.time_sec.size)
+            nc_group_timedep.createDimension('time_ls',      e5.time_sec.size)
+
+            add_nc_var(nc_group_timedep, 'time_surface', ('time_surface'), e5.time_sec)
+            add_nc_var(nc_group_timedep, 'time_ls',      ('time_ls'),      e5.time_sec)
+
+            # Write the time dependent surface variables
+            add_nc_var(nc_group_timedep, 'thl_sbot', ('time_surface'), e5.wth_mean)
+            add_nc_var(nc_group_timedep, 'qt_sbot',  ('time_surface'), e5.wq_mean)
+            add_nc_var(nc_group_timedep, 'p_sbot',   ('time_surface'), e5.ps_mean)
+
+            # Write the dime dependent geostrophic wind
+            add_nc_var(nc_group_timedep, 'u_geo',  ('time_ls', 'z'), ug)
+            add_nc_var(nc_group_timedep, 'v_geo',  ('time_ls', 'z'), vg)
+            add_nc_var(nc_group_timedep, 'w_ls',   ('time_ls', 'z'), w)
+
+            # Write the time dependent tendencies
+            add_nc_var(nc_group_timedep, 'thl_ls', ('time_ls', 'z'), thlls)
+            add_nc_var(nc_group_timedep, 'qt_ls',  ('time_ls', 'z'), qtls)
+            add_nc_var(nc_group_timedep, 'u_ls',   ('time_ls', 'z'), uls)
+            add_nc_var(nc_group_timedep, 'v_ls',   ('time_ls', 'z'), vls)
+
+            # Write the time dependent nudging profiles
+            add_nc_var(nc_group_init,    'nudgefac',  ('z'), nudge_fac)
+            add_nc_var(nc_group_timedep, 'thl_nudge', ('time_ls', 'z'), thl)
+            add_nc_var(nc_group_timedep, 'qt_nudge',  ('time_ls', 'z'), qt)
+            add_nc_var(nc_group_timedep, 'u_nudge',   ('time_ls', 'z'), u)
+            add_nc_var(nc_group_timedep, 'v_nudge',   ('time_ls', 'z'), v)
+
+            nc_file.close()
+
+        else:
+            error('NetCDF input only supported for MicroHH >=2.0', exit=True)
+
+    else:
+
+        # 2. Initial profiles
+        variables = {'z':grid.z, 'thl':thl[0,:], 'qt':qt[0,:], 'u':u[0,:], 'v':v[0,:], 'nudgefac':nudge_fac}
+        mht.write_output('testbed.prof', variables, grid.z.size)
+
+        # 3. Time varying nudging profiles
+        # Switch between old file names 1.x, and new ones in 2.0
+        if microhh_version == 1:
+            names = {'thl_nudge': 'thlnudge.timeprof', 'qt_nudge': 'qtnudge.timeprof',
+                     'u_nudge': 'unudge.timeprof', 'v_nudge': 'vnudge.timeprof',
+                     'thl_ls': 'thlls.timeprof', 'qt_ls': 'qtls.timeprof',
+                     'u_ls': 'uls.timeprof', 'v_ls': 'vls.timeprof',
+                     'ug': 'ug.timeprof', 'vg': 'vg.timeprof', 'wls': 'wls.timeprof'}
+
+        elif microhh_version == 2:
+            names = {'thl_nudge': 'thl_nudge.time', 'qt_nudge': 'qt_nudge.time',
+                     'u_nudge': 'u_nudge.time', 'v_nudge': 'v_nudge.time',
+                     'thl_ls': 'thl_ls.time', 'qt_ls': 'qt_ls.time',
+                     'u_ls': 'u_ls.time', 'v_ls': 'v_ls.time',
+                     'ug': 'u_geo.time', 'vg': 'v_geo.time', 'wls': 'w_ls.time'}
+
+        mht.write_time_profs(names['thl_nudge'], grid.z, e5.time_sec, thl)
+        mht.write_time_profs(names['qt_nudge'],  grid.z, e5.time_sec, qt )
+        mht.write_time_profs(names['u_nudge'],   grid.z, e5.time_sec, u  )
+        mht.write_time_profs(names['v_nudge'],   grid.z, e5.time_sec, v  )
+
+        # 4. Time varying large scale advective tendencies
+        mht.write_time_profs(names['thl_ls'], grid.z, e5.time_sec, thlls)
+        mht.write_time_profs(names['qt_ls'],  grid.z, e5.time_sec, qtls )
+        mht.write_time_profs(names['u_ls'],   grid.z, e5.time_sec, uls  )
+        mht.write_time_profs(names['v_ls'],   grid.z, e5.time_sec, vls  )
+
+        # 5. Time varying geostrophic wind and subsidence
+        mht.write_time_profs(names['ug'],  grid.z, e5.time_sec, ug)
+        mht.write_time_profs(names['vg'],  grid.z, e5.time_sec, vg)
+        mht.write_time_profs(names['wls'], grid.z, e5.time_sec, w)
+
+        # 7. Time varying surface variables
+        if microhh_version == 1:
+            variables = {'t': e5.time_sec, 'sbot[thl]': e5.wth_mean, 'sbot[qt]': e5.wq_mean, 'pbot': e5.ps_mean}
+            mht.write_output('testbed.time', variables, e5.time_sec.size)
+
+        elif microhh_version == 2:
+            mht.write_output('thl_sbot.time', {'time': e5.time_sec, 'thl_sbot': e5.wth_mean}, e5.time_sec.size)
+            mht.write_output('qt_sbot.time',  {'time': e5.time_sec, 'qt_sbot': e5.wq_mean}, e5.time_sec.size)
+            mht.write_output('p_sbot.time',   {'time': e5.time_sec, 'p_sbot': e5.ps_mean}, e5.time_sec.size)
