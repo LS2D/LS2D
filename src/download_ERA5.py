cdd3de47a3569a8aab993874d6e4b6a88bf0c9b3
julietbravo <bart@vanstratum.com>
2020-05-05 07:14:46 +0000

    Added ozone to ERA5 download

--- src/download_ERA5.py
+++ src/download_ERA5.py
@@ -142,9 +142,6 @@ def download_ERA5_file(settings):
     press_levels = '1/2/3/5/7/10/20/30/50/70/100/125/150/175/200/225/250/300/350/400/450/\
 500/550/600/650/700/750/775/800/825/850/875/900/925/950/975/1000'
 
-    #model_levels = '1/to/2/by/1'
-    #press_levels = '975/1000'
-
     an_times = '0/to/23/by/1'
     fc_times = '06:00:00/18:00:00'
     fc_steps = '1/to/12/by/1'
@@ -156,7 +153,7 @@ def download_ERA5_file(settings):
             'type'     : 'an',
             'levelist' : model_levels,
             'time'     : an_times,
-            'param'    : '75/76/129/130/131/132/133/135/152/246/247/248'
+            'param'    : '75/76/129/130/131/132/133/135/152/246/247/248/203'
         })
 
     elif settings['ftype'] == 'model_fc':
@@ -308,8 +305,10 @@ if __name__ == "__main__":
         #'base_path'   : '/Users/bart/meteo/data/LS2D/',        # Macbook
         #'base_path'   : '/home/scratch1/meteo_data/LS2D/',     # Arch
         'base_path'   : '/scratch/ms/nl/nkbs/LS2D/',            # ECMWF
-        'start_date'  : datetime.datetime(year=2018, month=8, day=11, hour=0),
-        'end_date'    : datetime.datetime(year=2018, month=8, day=12, hour=0),
+        #'start_date'  : datetime.datetime(year=2018, month=8, day=11, hour=0),
+        #'end_date'    : datetime.datetime(year=2018, month=8, day=12, hour=0),
+        'start_date'  : datetime.datetime(year=2016, month=8, day=04, hour=0),
+        'end_date'    : datetime.datetime(year=2016, month=8, day=18, hour=0),
         'write_log'   : False,
         'data_source' : 'MARS',
         'ntasks'      : 1
