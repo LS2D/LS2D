bbcab61bc47e85a2d6881642e9749c2b72a6e87b
julietbravo <bart@vanstratum.com>
2020-05-18 12:27:25 +0200

    Moved LES grid definition to separate Python script

---
+++ cases/grid.py
@@ -0,0 +1,105 @@
+import numpy as np
+
+#
+# Vertical grids
+#
+class Grid:
+    def __init__(self, kmax, dz0):
+        self.kmax = kmax
+        self.dz0  = dz0
+
+        self.z = np.zeros(kmax)
+        self.dz = np.zeros(kmax)
+        self.zsize = None
+
+    def plot(self):
+        pl.figure()
+        pl.title(r'$z_\mathrm{size} = {0:.1f} m'.format(self.zsize), loc='left')
+        pl.plot(self.dz, self.z, '-x')
+        pl.xlabel(r'$\Delta z$ (m)')
+        pl.ylabel(r'$z$ (m)')
+
+
+class Grid_equidist(Grid):
+    def __init__(self, kmax, dz0):
+        Grid.__init__(self, kmax, dz0)
+
+        self.zsize = kmax * dz0
+        self.z[:]  = np.arange(dz0/2, self.zsize, dz0)
+        self.dz[:] = dz0
+
+
+class Grid_stretched(Grid):
+    def __init__(self, kmax, dz0, nloc1, nbuf1, dz1, nloc2=None, nbuf2=None, dz2=None):
+        Grid.__init__(self, kmax, dz0)
+
+        double_stretched = nloc2 is not None and nbuf2 is not None and dz2 is not None
+
+        dn = 1./kmax
+        n = np.linspace(dn, 1.-dn, kmax)
+
+        nloc1 *= dn
+        nbuf1 *= dn
+
+        if double_stretched:
+            nloc2 *= dn
+            nbuf2 *= dn
+
+        dzdn1 = dz0/dn
+        dzdn2 = dz1/dn
+
+        if double_stretched:
+            dzdn3  = dz2/dn
+
+        if double_stretched:
+            dzdn = dzdn1 + 0.5*(dzdn2-dzdn1)*(1. + np.tanh((n-nloc1)/nbuf1)) + 0.5*(dzdn3-dzdn2)*(1. + np.tanh((n-nloc2)/nbuf2))
+        else:
+            dzdn = dzdn1 + 0.5*(dzdn2-dzdn1)*(1. + np.tanh((n-nloc1)/nbuf1))
+
+        self.dz[:] = dzdn*dn
+
+        stretch = np.zeros(self.dz.size)
+        self.z[0]  = 0.5*self.dz[0]
+        stretch[0] = 1.
+
+        for k in range(1, self.kmax):
+              self.z [k] = self.z[k-1] + 0.5*(self.dz[k-1]+self.dz[k])
+              stretch[k] = self.dz[k]/self.dz[k-1]
+
+        self.zsize = self.z[kmax-1] + 0.5*self.dz[kmax-1]
+
+
+class Grid_linear_stretched(Grid):
+    def __init__(self, kmax, dz0, alpha):
+        Grid.__init__(self, kmax, dz0)
+
+        self.dz[:] = dz0 * (1 + alpha)**np.arange(kmax)
+        zh         = np.zeros(kmax+1)
+        zh[1:]     = np.cumsum(self.dz)
+        self.z[:]  = 0.5 * (zh[1:] + zh[:-1])
+        self.zsize = zh[-1]
+
+
+if __name__ == '__main__':
+    """
+    Just for testing...
+    """
+    import matplotlib.pyplot as pl
+    pl.close('all'); pl.ion()
+
+    ktot = 224
+    dz0  = 20
+
+    equidist   = Grid_equidist(ktot, dz0)
+    linear     = Grid_linear_stretched(ktot, dz0, 0.01)
+    stretched1 = Grid_stretched(ktot, dz0, 90, 20, 150)
+    stretched2 = Grid_stretched(ktot, dz0, 100, 20, 100, 210, 10, 500)
+
+    pl.figure()
+    pl.plot(equidist.dz, equidist.z, '-x', label='equidistant')
+    pl.plot(linear.dz, linear.z, '-x', label='linear')
+    pl.plot(stretched1.dz, stretched1.z, '-x', label='stretched-single')
+    pl.plot(stretched2.dz, stretched2.z, '-x', label='stretched-double')
+    pl.legend()
+    pl.xlabel('dz (m)')
+    pl.ylabel('z (m)')
---
+++ cases/microhh_tools.py
@@ -0,0 +1,303 @@
+import netCDF4 as nc4
+import numpy   as np
+import struct  as st
+import glob
+import re
+
+# -------------------------
+# General help functions
+# -------------------------
+
+def _int_or_float_or_str(value):
+    """ Helper function: convert a string to int/float/str """
+    try:
+        if ('.' in value):
+            return float(value)
+        else:
+            return int(float(value))
+    except:
+        return value.rstrip()
+
+
+def _convert_value(value):
+    """ Helper function: convert namelist value or list """
+    if ',' in value:
+        value = value.split(',')
+        return [_int_or_float_or_str(val) for val in value]
+    else:
+        return _int_or_float_or_str(value)
+
+
+def _find_namelist_file():
+    """ Helper function: automatically find the .ini file in the current directory """
+    namelist_file = glob.glob('*.ini')
+    if len(namelist_file) == 0:
+        raise RuntimeError('Can\'t find any .ini files in the current directory!')
+    if len(namelist_file) > 1:
+        raise RuntimeError('There are multiple .ini files: {}'.format(namelist_file))
+    else:
+        return namelist_file[0]
+
+
+def _process_endian(endian):
+    if endian not in ['little', 'big']:
+        raise ValueError('endian has to be \"little\" or \"big\"!')
+    endian = '<' if endian == 'little' else '>'
+    return endian
+
+
+# -------------------------
+# Classes and functions to read and write MicroHH things
+# -------------------------
+
+def read_namelist(namelist_file=None):
+    """
+    Read a .ini namelist into a nested dictionary (dict[group][variable])
+    """
+
+    ini = {}
+    with open(namelist_file, 'r') as f:
+        for line in f:
+            lstrip = line.strip()
+            if (len(lstrip) > 0 and lstrip[0] != "#"):
+                if lstrip[0] == '[' and lstrip[-1] == ']':
+                    group_name = lstrip[1:-1]
+                    ini[group_name] = {}
+                elif ("=" in line):
+                    var_name = lstrip.split('=')[0]
+                    value = _convert_value(lstrip.split('=')[1])
+                    ini[group_name][var_name] = value
+    return ini
+
+def write_namelist(namelist_file, namelist_dict):
+    """
+    Write a .ini namelist from a (nested) dictionary
+    """
+
+    with open(namelist_file, 'w') as f:
+        for group, items in namelist_dict.items():
+            f.write('[{}]\n'.format(group))
+            for variable,value in items.items():
+                if isinstance(value, list):
+                    value = ','.join(value)
+                f.write('{}={}\n'.format(variable, value))
+            f.write('\n')
+
+
+class Read_statistics:
+    """ Read all the NetCDF statistics
+        Example:
+        f = Read_statistics('drycblles.default.0000000.nc')
+        print(f) prints a list with the available variables
+        The data can be accessed as either f['th'] or f.th, which returns the numpy array with data
+        The variable names can be accessed as f.names['th'], the units as f.units['th'], the dimensions as f.dimensions['th']
+        This allows you to automatically format axis labels as e.g.:
+        pl.xlabel("{0:} ({1:})".format(f.names['th'], f.units['th']))
+        """
+    def __init__(self, stat_file):
+        f = nc4.Dataset(stat_file, 'r')
+
+        # Dictionaries which hold the variable names, units, etc.
+        self.data       = {}
+        self.units      = {}
+        self.names      = {}
+        self.dimensions = {}
+
+        # For each variable in the NetCDF file, read all the content and info
+        for var in f.variables:
+            self.data[var]       = f.variables[var].__array__()
+            self.units[var]      = f.variables[var].units
+            self.names[var]      = f.variables[var].long_name
+            self.dimensions[var] = f.variables[var].dimensions
+
+        f.close()
+
+    def __getitem__(self, name):
+        if name in self.data.keys():
+            return self.data[name]
+        else:
+            raise RuntimeError('Can\'t find variable \"{}\" in statistics file'.format(name))
+
+    def __getattr__(self, name):
+        if name in self.data.keys():
+            return self.data[name]
+        else:
+            raise RuntimeError('Can\'t find variable \"{}\" in statistics file'.format(name))
+
+    def __repr__(self):
+        return 'Available variables:\n{}'.format(', '.join(self.names.keys()))
+
+
+class Read_grid:
+    """ Read the grid file from MicroHH.
+        If no file name is provided, grid.0000000 from the current directory is read """
+    def __init__(self, itot, jtot, ktot, zsize, filename=None, endian='little'):
+        self.en  = _process_endian(endian)
+        filename = 'grid.0000000' if filename is None else filename
+
+        self.fin = open(filename, 'rb')
+        self.x  = self.read(itot)
+        self.xh = self.read(itot)
+        self.y  = self.read(jtot)
+        self.yh = self.read(jtot)
+        self.z  = self.read(ktot)
+        self.zh = self.read(ktot)
+        self.fin.close()
+        del self.fin
+
+        self.itot = self.x.size
+        self.jtot = self.y.size
+        self.ktot = self.z.size
+
+        self.dx = self.x[1]-self.x[0] if itot > 1 else self.xh[1]
+        self.dy = self.y[1]-self.y[0] if jtot > 1 else self.yh[1]
+
+        self.xsize = self.itot * self.dx
+        self.ysize = self.jtot * self.dy
+        self.zsize = zsize
+
+    def read(self, n):
+        return np.array(st.unpack('{0}{1}d'.format(self.en, n), self.fin.read(n*8)))
+
+
+def read_restart_file(path, itot, jtot, ktot, endian='little'):
+    """ Read a MicroHH restart file into a 3D (or 2D if ktot=1) numpy array
+        The returned array has the dimensions ordered as [z,y,x] """
+
+    en = _process_endian(endian)
+
+    f  = open(path, 'rb')
+    if (ktot > 1):
+        field = np.zeros((ktot, jtot, itot))
+        for k in range(ktot):
+            raw = f.read(itot*jtot*8)
+            tmp = np.array(st.unpack('{0}{1}d'.format(en, itot*jtot), raw))
+            field[k,:,:] = tmp.reshape((jtot, itot))[:,:]
+        f.close()
+    else:
+        raw = f.read(itot*jtot*8)
+        tmp = np.array(st.unpack('{0}{1}d'.format(en, itot*jtot), raw))
+        field = tmp.reshape((jtot, itot))
+
+    return field
+
+
+def write_restart_file(data, itot, jtot, ktot, path, per_slice=True, endian='little'):
+    """ Write a restart file in the format requires by MicroHH.
+        The input array should be indexed as [z,y,x] """
+
+    en = _process_endian(endian)
+
+    if(per_slice):
+        # Write level by level (less memory hungry.....)
+        fout  = open(path, "wb")
+        for k in range(ktot):
+            tmp  = data[k,:,:].reshape(itot*jtot)
+            tmp2 = st.pack('{0}{1}d'.format(en, tmp.size), *tmp)
+            fout.write(tmp2)
+        fout.close()
+    else:
+        # Write entire field at once (memory hungry....)
+        tmp  = data.reshape(data.size)
+        tmp2 = st.pack('{0}{1}d'.format(en, tmp.size), *tmp)
+        fout = open(path, "wb")
+        fout.write(tmp2)
+        fout.close()
+
+
+def write_NetCDF_input(case_name, float_type, init_profiles, tdep_surface=None, tdep_ls=None, radiation=None, soil=None):
+    """
+    Function for writing the MicroHH2 NetCDF input
+    """
+
+    def add_variable(nc_group, name, dims, data, float_type):
+        var = nc_group.createVariable(name, float_type, dims)
+        var[:] = data[:]
+
+    # Define new NetCDF file
+    nc_file = nc4.Dataset('{}_input.nc'.format(case_name), mode='w', datamodel='NETCDF4')
+
+    # Create height dimension, and set height coordinate
+    nc_file.createDimension('z', init_profiles['z'].size)
+    add_variable(nc_file, 'z', ('z'), init_profiles['z'], float_type)
+
+    # Create a group called "init" for the initial profiles.
+    nc_group_init = nc_file.createGroup('init')
+
+    # Set the initial profiles
+    for name, data in init_profiles.items():
+        add_variable(nc_group_init, name, ('z'), data, float_type)
+
+    # Create a group called "timedep" for the time dependent input
+    if tdep_surface is not None or tdep_ls is not None:
+        nc_group_timedep = nc_file.createGroup('timedep')
+
+    # Write the time dependent surface values
+    if tdep_surface is not None:
+        nc_group_timedep.createDimension('time_surface', tdep_surface['time_surface'].size)
+
+        for name, data in tdep_surface.items():
+            add_variable(nc_group_timedep, name, ('time_surface'), data, float_type)
+
+    # Write the time dependent atmospheric values
+    if tdep_ls is not None:
+        nc_group_timedep.createDimension('time_ls', tdep_ls['time_ls'].size)
+
+        for name, data in tdep_ls.items():
+            dims = ('time_ls') if name == 'time_ls' else ('time_ls', 'z')
+            add_variable(nc_group_timedep, name, dims, data, float_type)
+
+    if radiation is not None:
+        nc_group_rad = nc_file.createGroup('radiation')
+
+        nc_group_rad.createDimension("lay", radiation['p_lay'].size)
+        nc_group_rad.createDimension("lev", radiation['p_lev'].size)
+
+        for name, data in radiation.items():
+            dims = ('lay') if data.size == radiation['p_lay'].size else ('lev')
+            add_variable(nc_group_rad, name, dims, data, float_type)
+
+    if soil is not None:
+        nc_group_soil = nc_file.createGroup('soil')
+        nc_group_soil.createDimension("z", soil['z'].size)
+
+        for name, data in soil.items():
+            add_variable(nc_group_soil, name, 'z', data, float_type)
+
+    nc_file.close()
+
+
+
+def get_cross_indices(variable, mode):
+    """ Find the cross-section indices given a variable name and mode (in 'xy','xz','yz') """
+    if mode not in ['xy','xz','yz']:
+        raise ValueError('\"mode\" should be in {\"xy\", \"xz\", \"yz\"}')
+
+    # Get a list of all the cross-section files
+    files = glob.glob('{}.{}.*.*'.format(variable, mode))
+    if len(files) == 0:
+        raise Exception('Cannot find any cross-section')
+
+    # Get a list with all the cross-section files for one time
+    time  = files[0].split('.')[-1]
+    files = glob.glob('{}.{}.*.{}'.format(variable, mode, time))
+
+    # Get the indices
+    indices = [int(f.split('.')[-2]) for f in files]
+    indices.sort()
+    return indices
+
+
+
+if (__name__ == "__main__"):
+
+    import matplotlib.pylab as pl
+    pl.close('all')
+    pl.ion()
+
+    #grid = Stretched_grid(kmax=140, nloc1=80, nbuf1=20, dz1=25, dz2=250)
+    #grid = Stretched_grid(kmax=512, nloc1=150, nbuf1=60, dz1=10, dz2=100)
+    grid = Stretched_grid(kmax=192, nloc1=140, nbuf1=30, dz1=20, dz2=300)
+    grid.plot()
+
+
