c5c85fef175753a950da53e3624c7e0e99113f8b
julietbravo <bart@vanstratum.com>
2020-05-23 12:02:00 +0200

    Tuning 11 Aug Cabauw case, plus upate validation script

--- cases/cabauw_20180811/LS2D.py
+++ cases/cabauw_20180811/LS2D.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as pl
+import xarray as xr
 import datetime
 import numpy as np
 import sys
@@ -19,6 +20,8 @@ from messages      import header, message, error
 import microhh_tools as mht
 from grid import Grid_stretched
 
+pl.close('all'); pl.ion()
+
 def execute(task):
    subprocess.call(task, shell=True, executable='/bin/bash')
 
@@ -35,13 +38,13 @@ env_arch = {
         'rrtmgp_path': '/home/bart/meteo/models/rte-rrtmgp/'}
 
 # Switch between different systems:
-env = env_cartesius
+env = env_arch
 
 float_type  = 'f8'    # MicroHH float type ('f4', 'f8')
 auto_submit = True    # Submit the case to load balancer
 link_files = False    # Switch between linking or copying files
-auto_submit = True    # Submit the case to load balancer
-set_lfs_stripe = True # Set the LFS striping on new directories
+auto_submit = False    # Submit the case to load balancer
+set_lfs_stripe = False # Set the LFS striping on new directories
 
 start = datetime.datetime(year=2018, month=8, day=11, hour=5)
 end   = start + datetime.timedelta(hours=13)
@@ -62,59 +65,68 @@ settings = {
 
 header('Creating LES input')
 
-#
 # Download the ERA5 data (or check whether it is available local).
-#
 download_ERA5(settings)
 
-#
 # Read ERA5 data, and calculate LES forcings, using +/-`n_av` grid point averages in ERA5.
-#
 e5 = Read_ERA(settings)
 e5.calculate_forcings(n_av=0, method='4th')
 
-#
 # Read MicroHH namelist and create stretched vertical grid
-#
 grid = Grid_stretched(kmax=228, dz0=20, nloc1=100, nbuf1=20, dz1=100, nloc2=210, nbuf2=10, dz2=500)
-#grid.plot()
 
-#
+# Interpolate ERA5 variables and forcings onto LES grid
+variables = [
+        'thl', 'qt', 'u', 'v', 'wls', 'p',
+        'dtthl_advec', 'dtqt_advec', 'dtu_advec', 'dtv_advec',
+        'ug' ,'vg' ,'o3', 'z']
+e5_at_z = e5.interpolate_to_fixed_height(variables, grid.z)
+
 # Create nudge factor, controlling where nudging is aplied, and time scale
-#
 tau_nudge = 10800        # Nudge time scale (s)
 nudge_fac = np.ones(grid.z.size) / tau_nudge
-
-#
-# Interpolate ERA5 onto LES grid
-#
-def interp_time(z, ze, arr):
-    out = np.empty((arr.shape[0], z.size))
-    for i in range(arr.shape[0]):
-        out[i,:] = np.interp(z, ze[i,:], arr[i,:])
-    return out
-
-thl   = interp_time(grid.z, e5.z_mean, e5.thl_mean   )
-qt    = interp_time(grid.z, e5.z_mean, e5.qt_mean    )
-u     = interp_time(grid.z, e5.z_mean, e5.u_mean     )
-v     = interp_time(grid.z, e5.z_mean, e5.v_mean     )
-w     = interp_time(grid.z, e5.z_mean, e5.wls_mean   )
-p     = interp_time(grid.z, e5.z_mean, e5.p_mean     )
-thlls = interp_time(grid.z, e5.z_mean, e5.dtthl_advec)
-qtls  = interp_time(grid.z, e5.z_mean, e5.dtqt_advec )
-uls   = interp_time(grid.z, e5.z_mean, e5.dtu_advec  )
-vls   = interp_time(grid.z, e5.z_mean, e5.dtv_advec  )
-ug    = interp_time(grid.z, e5.z_mean, e5.ug         )
-vg    = interp_time(grid.z, e5.z_mean, e5.vg         )
-o3    = interp_time(grid.z, e5.z_mean, e5.o3_mean    )
 
 # Surface / soil
 z_soil = np.array([-1.945, -0.64, -0.175, -0.035])
 soil_index = np.ones_like(z_soil)*2
 
-#
+if False:
+    # Debug/fix soil moisture ERA5
+    vg = xr.open_dataset('../van_genuchten_parameters.nc')
+
+    si = 2
+    theta_soil_rel_e5 = (e5.theta_soil_mean[0,:] - vg['theta_wp'][si].values)\
+            / (vg['theta_fc'][si].values - vg['theta_wp'][si].values)
+
+    theta_soil_new = np.array([0.35, 0.35, 0.14, 0.14])
+    theta_soil_rel_e5_new = (theta_soil_new - vg['theta_wp'][si].values)\
+            / (vg['theta_fc'][si].values - vg['theta_wp'][si].values)
+
+    z_cb = np.array([-0.05, -0.19, -0.33, -0.56])
+    theta_soil_cb = np.array([0.21, 0.20, 0.30, 0.48])
+    si0 = 16  # =O12 = fairly heavy clay (top soil)
+    si1 = 35  # =O12 = fairly heavy clay (lower soil)
+    theta_soil_rel_cb = np.zeros_like(theta_soil_cb)
+    theta_soil_rel_cb[0] = (theta_soil_cb[0] - vg['theta_wp'][si0].values)\
+            / (vg['theta_fc'][si0].values - vg['theta_wp'][si0].values)
+    theta_soil_rel_cb[1:] = (theta_soil_cb[1:] - vg['theta_wp'][si1].values)\
+            / (vg['theta_fc'][si1].values - vg['theta_wp'][si1].values)
+
+    pl.figure()
+    pl.subplot(121)
+    pl.plot(e5.theta_soil_mean[0,:], z_soil, '-x', label='ERA5')
+    pl.plot(theta_soil_new, z_soil, '--x', label='ERA5-new')
+    pl.plot(theta_soil_cb, z_cb, '-x', label='Cabauw')
+    pl.legend()
+
+    pl.subplot(122)
+    pl.plot(theta_soil_rel_e5, z_soil, '-x', label='ERA5')
+    pl.plot(theta_soil_rel_e5_new, z_soil, '--x', label='ERA5-new')
+    pl.plot(theta_soil_rel_cb, z_cb, '-x', label='Cabauw')
+    pl.xlim(0,1)
+    pl.grid()
+
 # Radiation profiles for RRTMGP
-#
 co2 = 348.e-6
 ch4 = 1650.e-9
 n2o = 306.e-9
@@ -144,22 +156,18 @@ o2_rad  = np.ones(e5.nfull) * o2
 o3_rad  = e5.o3_mean[0,:]
 
 # Profiles on LES grid
-h2o_atmo = qt[0,:]
+h2o_atmo = e5_at_z['qt'][0,:]
 co2_atmo = np.ones(grid.kmax) * co2
 ch4_atmo = np.ones(grid.kmax) * ch4
 n2o_atmo = np.ones(grid.kmax) * n2o
 n2_atmo  = np.ones(grid.kmax) * n2
 o2_atmo  = np.ones(grid.kmax) * o2
-o3_atmo  = o3[0,:]
+o3_atmo  = e5_at_z['o3'][0,:]
 
-#
 # Write MicroHH input
-#
 message('Writing forcings as LES input')
 
-#
 # Update namelist variables
-#
 nl_file = '{}.ini'.format(settings['case_name'])
 nl_backup = '{}.ini.bak'.format(settings['case_name'])
 
@@ -190,9 +198,10 @@ mht.write_namelist(nl_file, nl)
 # Write NetCDF file
 #
 init_profiles = {
-        'z': grid.z, 'thl': thl[0,:], 'qt': qt[0,:], 'u': u[0,:],
-        'v': v[0,:], 'nudgefac': nudge_fac, 'co2': co2_atmo, 'ch4': ch4_atmo,
-        'n2o': n2o_atmo, 'n2': n2_atmo, 'o2': o2_atmo, 'o3': o3_atmo, 'h2o': h2o_atmo}
+        'z': grid.z, 'thl': e5_at_z['thl'][0,:], 'qt': e5_at_z['qt'][0,:],
+        'u': e5_at_z['u'][0,:], 'v': e5_at_z['v'][0,:], 'nudgefac': nudge_fac,
+        'co2': co2_atmo, 'ch4': ch4_atmo, 'n2o': n2o_atmo, 'n2': n2_atmo,
+        'o2': o2_atmo, 'o3': o3_atmo, 'h2o': h2o_atmo}
 
 radiation  = {
         'z_lay': z_lay, 'z_lev': z_lev, 'p_lay': p_lay, 'p_lev': p_lev,
@@ -204,14 +213,17 @@ tdep_surface = {
         'qt_sbot': e5.wqs_mean, 'p_sbot': e5.ps_mean }
 
 tdep_ls = {
-        'time_ls': e5.time_sec, 'u_geo': ug, 'v_geo': vg, 'w_ls': w,
-        'thl_ls': thlls, 'qt_ls': qtls, 'u_ls': uls, 'v_ls': vls,
-        'thl_nudge': thl, 'qt_nudge': qt, 'u_nudge': u, 'v_nudge': v}
+        'time_ls': e5.time_sec, 'u_geo': e5_at_z['ug'], 'v_geo': e5_at_z['vg'],
+        'w_ls': e5_at_z['wls'], 'thl_ls': e5_at_z['dtthl_advec'], 'qt_ls': e5_at_z['dtqt_advec'],
+        'u_ls': e5_at_z['dtu_advec'], 'v_ls': e5_at_z['dtv_advec'],
+        'thl_nudge': e5_at_z['thl'], 'qt_nudge': e5_at_z['qt'],
+        'u_nudge': e5_at_z['u'], 'v_nudge': e5_at_z['v']}
 
-soil = {'z': z_soil, 'theta': e5.phisoil_mean[0,::-1], 't': e5.Tsoil_mean[0,::-1], 'index': soil_index}
+theta_soil_fix = np.array([0.35, 0.35, 0.14, 0.14])
+soil = {'z': z_soil, 'theta': theta_soil_fix, 't': e5.T_soil_mean[0,::-1], 'index': soil_index}
+#soil = {'z': z_soil, 'theta': e5.theta_soil_mean[0,::-1], 't': e5.T_soil_mean[0,::-1], 'index': soil_index}
 
 mht.write_NetCDF_input('cabauw', float_type, init_profiles, tdep_surface, tdep_ls, radiation, soil)
-
 #
 # Copy/move/link files to working directory.
 #
--- cases/cabauw_20180811/validate.py
+++ cases/cabauw_20180811/validate.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as pl
 import matplotlib.dates as mdates
 
 import xarray as xr
+import pandas as pd
 import numpy as np
 from datetime import datetime
 
@@ -18,7 +19,7 @@ def format_ax(major, minor, fmt='%H:%M', ax=None):
     ax.xaxis.set_major_locator(major_loc)
     ax.xaxis.set_major_formatter(mdates.DateFormatter(fmt))
 
-    pl.grid()
+    #pl.grid()
 
 def exner(p):
     Rd = 287.04; cp = 1005.; p0 = 1e5
@@ -36,14 +37,8 @@ def interp_z(array, z, z_goal):
 
 if __name__ == '__main__':
 
-    start = datetime(2016, 8, 11, 5)
-    end   = datetime(2016, 8, 11, 19)
-
-    #start = datetime(2016, 8, 17, 5)
-    #end   = datetime(2016, 8, 17, 19)
-
-    #start = datetime(2018, 8, 11, 5)
-    #end   = datetime(2018, 8, 11, 19)
+    start = datetime(2018, 8, 11, 5)
+    end   = datetime(2018, 8, 11, 18)
 
     date_str = '{0:04d}-{1:02d}-{2:02d} (UTC)'.format(start.year, start.month, start.day)
 
@@ -74,26 +69,252 @@ if __name__ == '__main__':
     #
     # Read MicroHH experiment
     #
-    #nc = 'tmp.nc'
-    #nc = '{0:04d}{1:02d}{2:02d}_t{3:02d}/cabauw.default.0000000.nc'.format(
-    #        start.year, start.month, start.day, start.hour)
-    nc = '{0:04d}{1:02d}{2:02d}_t{3:02d}/tmp.nc'.format(
-            start.year, start.month, start.day, start.hour)
-
+    mhh_path = '/home/scratch1/meteo_data/LS2D/MicroHH_results/202005_144x144_24h/20180811/'
+    nc = f'{mhh_path}/cabauw.default.0000000.nc'
     groups = ['', 'default', 'radiation', 'thermo', 'land_surface', 'tend']
     dss = []
     for group in groups:
         dss.append( xr.open_dataset(nc, group=group) )
     ds = xr.merge(dss)
 
-    def calc_mean(ds, var):
-        ds[var] = ds['c_veg']  * ds['{}_veg'.format(var)] +\
-                  ds['c_soil'] * ds['{}_soil'.format(var)] +\
-                  ds['c_wet']  * ds['{}_wet'.format(var)]
+    # Read column statistics
+    col_ij = [52,92]
+    cols = []
+    for i in col_ij:
+        for j in col_ij:
+            cols.append(xr.open_dataset('{0}/cabauw_column_{1:05d}_{2:05d}_0000000.nc'.format(mhh_path,i,j)))
 
-    calc_mean(ds, 'H')
-    calc_mean(ds, 'LE')
-    calc_mean(ds, 'G')
+
+    def plot_columns(cols, variable, surface=False):
+        cc = pl.cm.viridis(np.linspace(0,1,len(cols)))
+        for i,c in enumerate(cols):
+            data = c[variable][:] if surface else c[variable][:,0]
+            pl.plot(c['time'], data, color=cc[i], alpha=0.4, linewidth=0.5)
+
+    c_mhh = '#19334c'
+
+    if True:
+        #
+        # Radiation
+        #
+        pl.figure(figsize=(8,5))
+        pl.subplot(221)
+        plot_columns(cols, 'sw_flux_dn')
+        pl.plot(ds.time, ds.sw_flux_dn[:,0], color=c_mhh, label=r'$\mu$HH')
+        pl.scatter(cb_rad.index, cb_rad.SWD, s=10, marker='o', facecolor='none', alpha=0.5, color='k', label='Cabauw')
+        pl.ylabel(r'$SW_\mathrm{in}$ (W m$^{-2}$)')
+        pl.ylim(0,1000)
+        pl.legend(ncol=2)
+        format_ax(major=3, minor=1)
+        pl.legend()
+
+        pl.subplot(222)
+        plot_columns(cols, 'sw_flux_up')
+        pl.plot(ds.time, ds.sw_flux_up[:,0], color=c_mhh)
+        pl.scatter(cb_rad.index, cb_rad.SWU, s=10, marker='o', facecolor='none', alpha=0.5, color='k')
+        pl.ylabel(r'$SW_\mathrm{out}$ (W m$^{-2}$)')
+        format_ax(major=3, minor=1)
+
+        pl.subplot(223)
+        plot_columns(cols, 'lw_flux_dn')
+        pl.plot(ds.time, ds.lw_flux_dn[:,0], color=c_mhh)
+        pl.scatter(cb_rad.index, cb_rad.LWD, s=10, marker='o', facecolor='none', alpha=0.5, color='k')
+        pl.ylabel(r'$LW_\mathrm{in}$ (W m$^{-2}$)')
+        pl.xlabel(date_str)
+        format_ax(major=3, minor=1)
+
+        pl.subplot(224)
+        plot_columns(cols, 'lw_flux_up')
+        pl.plot(ds.time, ds.lw_flux_up[:,0], color=c_mhh)
+        pl.scatter(cb_rad.index, cb_rad.LWU, s=10, marker='o', facecolor='none', alpha=0.5, color='k')
+        pl.ylabel(r'$LW_\mathrm{out}$ (W m$^{-2}$)')
+        pl.xlabel(date_str)
+        format_ax(major=3, minor=1)
+
+        pl.tight_layout()
+
+    if True:
+        #
+        # Surface fluxes
+        #
+        pl.figure(figsize=(8,4))
+        pl.subplot(131)
+        plot_columns(cols, 'H', True)
+        pl.plot(ds.time, ds.H, color=c_mhh, label=r'$\mu$HH')
+        pl.scatter(cb_flux.index, cb_flux.H, s=10, marker='o', facecolor='none', alpha=0.5, color='k', label='Cabauw')
+        pl.ylabel(r'$H$ (W m$^{-2}$)')
+        pl.xlabel(date_str)
+        pl.legend(ncol=1)
+        format_ax(major=6, minor=1)
+
+        pl.subplot(132)
+        plot_columns(cols, 'LE', True)
+        pl.plot(ds.time, ds.LE, color=c_mhh)
+        pl.scatter(cb_flux.index, cb_flux.LE, s=10, marker='o', facecolor='none', alpha=0.5, color='k')
+        pl.ylabel(r'$LE$ (W m$^{-2}$)')
+        pl.xlabel(date_str)
+        format_ax(major=6, minor=1)
+
+        pl.subplot(133)
+        plot_columns(cols, 'G', True)
+        pl.plot(ds.time, ds.G/2., color=c_mhh)
+        pl.scatter(cb_flux.index, -cb_flux.G0, s=10, marker='o', facecolor='none', alpha=0.5, color='k')
+        pl.ylabel(r'$G$ (W m$^{-2}$)')
+        pl.xlabel(date_str)
+        format_ax(major=6, minor=1)
+
+        pl.tight_layout()
+
+
+    if False:
+        #
+        # Atmospheric profiles
+        #
+        times = [datetime(2018,8,11,7), datetime(2018,8,11,10), datetime(2018,8,11,13)]
+        cc = ['tab:blue', 'tab:red', 'tab:green']
+
+        def sel_times(t):
+            ds_t = ds.      sel({'time': t}, method='nearest')
+            cb_t = cb_tower.sel({'time': t}, method='nearest')
+            return ds_t, cb_t
+
+        def date_str(date):
+            date = pd.to_datetime(date)
+            return '{0:04d}-{1:02d}-{2:02d} {3:02d}:{4:02d} UTC'.format(
+                    date.year, date.month, date.day, date.hour, date.minute)
+
+        pl.figure(figsize=(8,5))
+        pl.subplot(221)
+        for i,t in enumerate(times):
+            ds_t, cb_t = sel_times(t)
+            pl.plot   (ds_t.thl*exner(ds_t.phydro), ds_t.z, color=cc[i], label=date_str(ds_t.time.values))
+            pl.scatter(cb_t.TA, cb_t.z, s=10, marker='o', facecolor='none', color=cc[i])
+        pl.xlabel(r'$T$ (K)')
+        pl.ylabel(r'$z$ (m)')
+        pl.ylim(0,210)
+        pl.xlim(287,293)
+        pl.legend(fontsize=8)
+
+        pl.subplot(222)
+        for i,t in enumerate(times):
+            ds_t, cb_t = sel_times(t)
+            pl.plot   (ds_t.qt*1000, ds_t.z, color=cc[i])
+            pl.scatter(cb_t.Q, cb_t.z, s=10, marker='o', facecolor='none', color=cc[i])
+        pl.xlabel(r'$q_\mathrm{t}$ (g kg$^{-1}$)')
+        pl.ylim(0,210)
+        pl.xlim(6,8.5)
+
+
+
+    if False:
+        #
+        # Atmospheric variables
+        #
+        T  = np.zeros((ds.time.size, cb_tower.z.size))
+        qt = np.zeros((ds.time.size, cb_tower.z.size))
+
+        z_cb = np.array([10,20,40,80,140,200])
+
+        for k in range(z_cb.size):
+            p       = interp_z(ds.phydro.values, ds.z.values, z_cb[k])
+            print(z_cb[k], p.mean())
+            T [:,k] = interp_z(ds.thl.values, ds.z.values, z_cb[k]) * exner(p)
+            qt[:,k] = interp_z(ds.qt.values, ds.z.values, z_cb[k])
+
+        cc = pl.cm.PiYG(np.array([0,0.15,0.30,0.70,0.85,1]))
+
+        pl.figure(figsize=(8,4))
+        pl.subplot(121)
+        for k in range(z_cb.size):
+            pl.plot(ds.time, T[:,k], color=cc[k], label=r'$z$={}m'.format(z_cb[k]))
+            pl.scatter(cb_tower.time, cb_tower.TA[:,k_cb(z_cb[k])], s=10, marker='o', facecolor='none', color=cc[k])
+        pl.ylabel(r'$T$ (K)')
+        pl.legend()
+        format_ax(major=3, minor=1)
+        pl.xlabel(date_str)
+
+        pl.subplot(122)
+        for k in range(z_cb.size):
+            pl.plot(ds.time, qt[:,k]*1000, color=cc[k])
+            pl.scatter(cb_tower.time, cb_tower.Q[:,k_cb(z_cb[k])], s=10, marker='o', facecolor='none', color=cc[k])
+        pl.ylabel(r'$q$ (g kg$^{-1}$)')
+        format_ax(major=3, minor=1)
+        pl.xlabel(date_str)
+
+        pl.tight_layout()
+
+
+
+
+    if False:
+        #
+        # ASDF
+        #
+
+        pl.figure(figsize=(7,8))
+        ax=pl.subplot(511)
+        pl.plot(ds.time, ds.rr*1000*3600)
+        pl.ylabel(r'Rain rate (mm h$^{-1}$)')
+        ax.set_xticklabels([])
+
+        ax=pl.subplot(512)
+        pl.plot(ds.time, ds.c_veg, label='vegetation')
+        pl.plot(ds.time, ds.c_soil, label='bare soil')
+        pl.plot(ds.time, ds.c_wet, label='wet skin')
+        pl.ylabel('Fraction (-)')
+        ax.set_xticklabels([])
+        pl.legend()
+        pl.ylim(0,1)
+
+        ax=pl.subplot(513)
+        pl.plot(ds.time, ds.wl*1000)
+        pl.ylabel(r'$w_1$ (mm)')
+        ax.set_xticklabels([])
+        pl.legend()
+
+        ax=pl.subplot(514)
+        pl.plot(ds.time, ds.LE_veg*ds.c_veg, label='vegetation')
+        pl.plot(ds.time, ds.LE_soil*ds.c_soil, label='soil')
+        pl.plot(ds.time, ds.LE_wet*ds.c_wet, label='wet_skin')
+        pl.ylabel(r'LE (W m$^{-2}$)')
+        ax.set_xticklabels([])
+        pl.legend()
+
+        pl.subplot(515)
+        pl.plot(ds.time, ds.theta[:,-1])
+        pl.ylabel(r'$\theta_\mathrm{soil}$ (-)')
+
+        pl.tight_layout()
+
+
+
+
+
+
+
+
+
+    if False:
+        #
+        # Soil temperature and moisture
+        #
+        pl.figure(figsize=(8,3))
+        pl.subplot(121)
+        pl.plot(f.time, fs.t[:,-1], label='1 (top)')
+        pl.plot(f.time, fs.t[:,-2], label='2')
+        pl.plot(f.time, fs.t[:,-3], label='3')
+        pl.plot(f.time, fs.t[:,-4], label='4 (bottom)')
+        pl.ylabel(r'$T_\mathrm{soil}$ (K)')
+        pl.legend()
+
+        pl.subplot(122)
+        pl.plot(f.time, fs.theta[:,-1])
+        pl.plot(f.time, fs.theta[:,-2])
+        pl.plot(f.time, fs.theta[:,-3])
+        pl.plot(f.time, fs.theta[:,-4])
+        pl.ylabel(r'$\theta_\mathrm{soil}$ (-)')
+
+        pl.tight_layout()
 
 
     if False:
@@ -136,193 +357,3 @@ if __name__ == '__main__':
                 pl.plot(ds.time, getattr(ds, var)[:,k]*3600, label=t)
         pl.legend()
         pl.ylabel(r'$\partial_t v$ (m s$^{-1}$ h$^{-1}$)')
-
-
-
-    if True:
-        #
-        # ASDF
-        #
-
-        pl.figure(figsize=(7,8))
-        ax=pl.subplot(511)
-        pl.plot(ds.time, ds.rr*1000*3600)
-        pl.ylabel(r'Rain rate (mm h$^{-1}$)')
-        ax.set_xticklabels([])
-
-        ax=pl.subplot(512)
-        pl.plot(ds.time, ds.c_veg, label='vegetation')
-        pl.plot(ds.time, ds.c_soil, label='bare soil')
-        pl.plot(ds.time, ds.c_wet, label='wet skin')
-        pl.ylabel('Fraction (-)')
-        ax.set_xticklabels([])
-        pl.legend()
-        pl.ylim(0,1)
-
-        ax=pl.subplot(513)
-        pl.plot(ds.time, ds.wl*1000)
-        pl.ylabel(r'$w_1$ (mm)')
-        ax.set_xticklabels([])
-        pl.legend()
-
-        ax=pl.subplot(514)
-        pl.plot(ds.time, ds.LE_veg*ds.c_veg, label='vegetation')
-        pl.plot(ds.time, ds.LE_soil*ds.c_soil, label='soil')
-        pl.plot(ds.time, ds.LE_wet*ds.c_wet, label='wet_skin')
-        pl.ylabel(r'LE (W m$^{-2}$)')
-        ax.set_xticklabels([])
-        pl.legend()
-
-        pl.subplot(515)
-        pl.plot(ds.time, ds.theta[:,-1])
-        pl.ylabel(r'$\theta_\mathrm{soil}$ (-)')
-
-        pl.tight_layout()
-
-
-
-    if False:
-        #
-        # Atmospheric variables
-        #
-        T  = np.zeros((ds.time.size, cb_tower.z.size))
-        qt = np.zeros((ds.time.size, cb_tower.z.size))
-
-        z_cb = np.array([10,20,40,80,140,200])
-
-        for k in range(z_cb.size):
-            p = interp_z(ds.phydro.values, ds.z.values, z_cb[k])
-            T [:,k] = interp_z(ds.thl.values, ds.z.values, z_cb[k]) * exner(p)
-            qt[:,k] = interp_z(ds.qt.values, ds.z.values, z_cb[k])
-
-        cc = pl.cm.PiYG(np.array([0,0.15,0.30,0.70,0.85,1]))
-
-        pl.figure(figsize=(8,5))
-        pl.subplot(121)
-        for k in range(z_cb.size):
-            pl.plot(ds.time, T[:,k], color=cc[k], label=r'$z$={}m'.format(z_cb[k]))
-            pl.scatter(cb_tower.time, cb_tower.TA[:,k_cb(z_cb[k])], s=10, marker='o', facecolor='none', color=cc[k])
-        pl.ylabel(r'$T$ (K)')
-        pl.legend()
-        format_ax(major=3, minor=1)
-        pl.xlabel(date_str)
-
-        pl.subplot(122)
-        for k in range(z_cb.size):
-            pl.plot(ds.time, qt[:,k]*1000, color=cc[k])
-            pl.scatter(cb_tower.time, cb_tower.Q[:,k_cb(z_cb[k])], s=10, marker='o', facecolor='none', color=cc[k])
-        pl.ylabel(r'$q$ (g kg$^{-1}$)')
-        format_ax(major=3, minor=1)
-        pl.xlabel(date_str)
-
-        pl.tight_layout()
-
-
-    if False:
-        #
-        # Near-surface / surface / soil
-        #
-
-        exn  = exner(ds.phydro [:,0])
-        exns = exner(ds.phydroh[:,0])
-
-        pl.figure(figsize=(8,5))
-        pl.subplot(111)
-        pl.plot(ds.time, ds.thl[:,0]*exn, color='tab:red', label=r'$z_1$')
-        pl.plot(ds.time, ds.thl_bot*exns, color='tab:green', label=r'surface')
-        pl.plot(ds.time, ds.t[:,-1], color='tab:blue', label=r'soil')
-        format_ax(major=3, minor=1)
-        pl.xlabel(date_str)
-        pl.legend()
-
-
-    if False:
-        #
-        # Radiation
-        #
-        pl.figure(figsize=(8,6))
-        pl.subplot(221)
-        pl.plot(ds.time, ds.sw_flux_dn[:,0], color='tab:red', label=r'$\mu$HH')
-        pl.scatter(cb_rad.index, cb_rad.SWD, s=10, marker='o', facecolor='none', alpha=0.5, color='k', label='Cabauw')
-        pl.ylabel(r'$SW_\mathrm{in}$ (W m$^{-2}$)')
-        pl.ylim(0,1000)
-        pl.legend(ncol=2)
-        format_ax(major=3, minor=1)
-        pl.legend()
-
-        pl.subplot(222)
-        pl.plot(ds.time, ds.sw_flux_up[:,0], color='tab:red')
-        pl.scatter(cb_rad.index, cb_rad.SWU, s=10, marker='o', facecolor='none', alpha=0.5, color='k')
-        pl.ylabel(r'$SW_\mathrm{out}$ (W m$^{-2}$)')
-        format_ax(major=3, minor=1)
-
-        pl.subplot(223)
-        pl.plot(ds.time, ds.lw_flux_dn[:,0], color='tab:red')
-        pl.scatter(cb_rad.index, cb_rad.LWD, s=10, marker='o', facecolor='none', alpha=0.5, color='k')
-        pl.ylabel(r'$LW_\mathrm{in}$ (W m$^{-2}$)')
-        pl.xlabel(date_str)
-        format_ax(major=3, minor=1)
-
-        pl.subplot(224)
-        pl.plot(ds.time, ds.lw_flux_up[:,0], color='tab:red')
-        pl.scatter(cb_rad.index, cb_rad.LWU, s=10, marker='o', facecolor='none', alpha=0.5, color='k')
-        pl.ylabel(r'$LW_\mathrm{out}$ (W m$^{-2}$)')
-        pl.xlabel(date_str)
-        format_ax(major=3, minor=1)
-
-        pl.tight_layout()
-        pl.savefig('rad_{}.png'.format(date_str))
-
-    if False:
-        #
-        # Surface fluxes
-        #
-        pl.figure(figsize=(9,4))
-        pl.subplot(131)
-        pl.plot(ds.time, ds.H, color='tab:red', label=r'$\mu$HH')
-        pl.scatter(cb_flux.index, cb_flux.H, s=10, marker='o', facecolor='none', alpha=0.5, color='k', label='Cabauw')
-        pl.ylabel(r'$H$ (W m$^{-2}$)')
-        pl.xlabel(date_str)
-        pl.legend(ncol=1)
-        format_ax(major=3, minor=1)
-
-        pl.subplot(132)
-        pl.plot(ds.time, ds.LE, color='tab:red')
-        pl.scatter(cb_flux.index, cb_flux.LE, s=10, marker='o', facecolor='none', alpha=0.5, color='k')
-        pl.ylabel(r'$LE$ (W m$^{-2}$)')
-        pl.xlabel(date_str)
-        format_ax(major=3, minor=1)
-
-        pl.subplot(133)
-        pl.plot(ds.time, ds.G, color='tab:red')
-        pl.scatter(cb_flux.index, -cb_flux.G0, s=10, marker='o', facecolor='none', alpha=0.5, color='k')
-        pl.ylabel(r'$G$ (W m$^{-2}$)')
-        pl.xlabel(date_str)
-        format_ax(major=3, minor=1)
-
-        pl.tight_layout()
-        pl.savefig('flux_{}.png'.format(date_str))
-
-
-    if False:
-        #
-        # Soil temperature and moisture
-        #
-        pl.figure(figsize=(8,3))
-        pl.subplot(121)
-        pl.plot(f.time, fs.t[:,-1], label='1 (top)')
-        pl.plot(f.time, fs.t[:,-2], label='2')
-        pl.plot(f.time, fs.t[:,-3], label='3')
-        pl.plot(f.time, fs.t[:,-4], label='4 (bottom)')
-        pl.ylabel(r'$T_\mathrm{soil}$ (K)')
-        pl.legend()
-
-        pl.subplot(122)
-        pl.plot(f.time, fs.theta[:,-1])
-        pl.plot(f.time, fs.theta[:,-2])
-        pl.plot(f.time, fs.theta[:,-3])
-        pl.plot(f.time, fs.theta[:,-4])
-        pl.ylabel(r'$\theta_\mathrm{soil}$ (-)')
-
-        pl.tight_layout()
-
