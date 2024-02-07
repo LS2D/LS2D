Fixed `read_era5.py`, and added debug print about averaging area.

examples/example_cams.py
@@ -27,13 +27,12 @@ import numpy as np
 
 # Only necessary if (LS)2D is not installed.
 sys.path.append('/home/bart/meteo/models/LS2D')
-
 import ls2d
 
 env = {
         'system': 'arch',
-        'era5_path': '/home/scratch1/bart/LS2D_ERA5/',
-        'cams_path': '/home/scratch1/bart/LS2D_CAMS/',
+        'era5_path': '/home/scratch1/meteo_data/LS2D_ERA5/',
+        'cams_path': '/home/scratch1/meteo_data/LS2D_CAMS/',
         'cdsapirc': '/home/bart/.cdsapirc'}
 
 cams_vars = {
@@ -62,8 +61,8 @@ cams_vars = {
 settings = {
     'central_lon'   : 4.92,
     'central_lat'   : 51.97,
-    'start_date'    : datetime(year=2016, month=8, day=15, hour=0),
-    'end_date'      : datetime(year=2016, month=8, day=16, hour=0),
+    'start_date'    : datetime(year=2016, month=8, day=15, hour=6),
+    'end_date'      : datetime(year=2016, month=8, day=15, hour=18),
     'area_size'     : 1,
     'case_name'     : 'cabauw',
     'cams_path'     : env['cams_path'],
@@ -82,8 +81,8 @@ ls2d.download_cams(settings)
 
 # Read, average over 3x3 grid points, and interpolate on LES grid.
 cams = ls2d.Read_cams(settings)
-z = np.linspace(5, 8000, 128)
-les_input = cams.get_les_input(z, n_av=1)
+z = np.arange(10, 5000, 20).astype(float)
+les_input = cams.get_les_input(z, n_av=0)
 
 # Quick plot.
 pl.figure(figsize=(10,8))

examples/example_era5.py
@@ -36,18 +36,14 @@ sys.path.append('/home/bart/meteo/models/LS2D')
 import ls2d
 
 settings = {
-    #'central_lat' : 51.971,
-    #'central_lon' : 4.927,
-    #'central_lat' : 51.9,
-    #'central_lon' : 4.45,
-    'central_lat' : 52.17,
-    'central_lon' : 5.74,
+    'central_lat' : 51.97,
+    'central_lon' : 4.93,
     'area_size'   : 1,
     'case_name'   : 'cabauw',
-    'era5_path'   : '/home/scratch1/meteo_data/LS2D/',
+    'era5_path'   : '/home/scratch1/meteo_data/LS2D_ERA5/',
     'era5_expver' : 1,   # 1=normal ERA5, 5=ERA5 near-realtime
-    'start_date'  : datetime(year=2022, month=5, day=17, hour=0),
-    'end_date'    : datetime(year=2022, month=5, day=19, hour=0),
+    'start_date'  : datetime(year=2016, month=8, day=15, hour=6),
+    'end_date'    : datetime(year=2016, month=8, day=15, hour=18),
     'write_log'   : False,
     'data_source' : 'CDS'
     }
@@ -61,14 +57,14 @@ era = ls2d.Read_era5(settings)
 # Calculate large-scale forcings:
 # `n_av` is the number of ERA5 gridpoints (+/-) over which
 # the ERA5 variables and forcings are averaged.
-era.calculate_forcings(n_av=0, method='2nd')
+era.calculate_forcings(n_av=1, method='2nd')
 
 # Interpolate ERA5 to fixed height grid:
 z = np.arange(10, 5000, 20).astype(float)
 les_input = era.get_les_input(z)
 
 # `les_input` is an xarray.Dataset, which can easily be save to NetCDF:
-les_input.to_netcdf('ls2d_loobos.nc')
+les_input.to_netcdf('ls2d.nc')
 
 # Plot variables as example:
 nrow = 5

ls2d/ecmwf/read_era5.py
@@ -41,6 +41,8 @@ Rd = 287.04
 Rv = 461.5
 ep = Rd/Rv
 
+ifs_tools = IFS_tools('L137')
+
 
 class Slice:
     def __init__(self, istart, iend, jstart, jend):
@@ -253,7 +255,7 @@ class Read_era5:
         self.theta_soil4 = get_variable(self.fsa, 'swvl4', s2d)  # Bottom soil layer moistsure (-)
 
         # Pressure level data:
-        self.z_p = get_variable(self.fpa, 'z', s3d) / IFS_tools.grav  # Geopotential height on pressure levels (m)
+        self.z_p = get_variable(self.fpa, 'z', s3d) / ifs_tools.grav  # Geopotential height on pressure levels (m)
         self.p_p = get_variable(self.fpa, 'level', s1d) * 100         # Pressure levels (Pa)
 
         # Convert ozone from mass mixing ratio to volume mixing ratio
@@ -268,7 +270,7 @@ class Read_era5:
 
         self.ql  = self.qc + self.qi + self.qr + self.qs  # Total liquid/solid specific humidity (kg kg-1)
         self.qt  = self.q + self.ql                       # Total specific humidity (kg kg-1)
-        self.Tv  = IFS_tools.calc_virtual_temp(
+        self.Tv  = ifs_tools.calc_virtual_temp(
                 self.T, self.q, self.qc, self.qi, self.qr, self.qs)  # Virtual temp on full levels (K)
 
         # Calculate half level pressure and heights
@@ -279,25 +281,25 @@ class Read_era5:
         for t in range(self.ntime):
             for la in range(self.nlat):
                 for lo in range(self.nlon):
-                    self.ph[t,:,la,lo] = IFS_tools.calc_half_level_pressure(self.ps[t,la,lo])
-                    self.zh[t,:,la,lo] = IFS_tools.calc_half_level_Zg(self.ph[t,:,la,lo], self.Tv[t,:,la,lo])
+                    self.ph[t,:,la,lo] = ifs_tools.calc_half_level_pressure(self.ps[t,la,lo])
+                    self.zh[t,:,la,lo] = ifs_tools.calc_half_level_Zg(self.ph[t,:,la,lo], self.Tv[t,:,la,lo])
 
         # Full level pressure and height as interpolation of the half level values
         self.p  = 0.5 * (self.ph[:,1:,:,:] + self.ph[:,:-1:,:])  # Full level pressure (Pa)
         self.z  = 0.5 * (self.zh[:,1:,:,:] + self.zh[:,:-1:,:])  # Full level height (m)
 
         # Other derived quantities
-        self.exn  = IFS_tools.calc_exner(self.p)  # Exner on full model levels (-)
+        self.exn  = ifs_tools.calc_exner(self.p)  # Exner on full model levels (-)
         self.th   = (self.T / self.exn)  # Potential temperature (K)
-        self.thl  = self.th - IFS_tools.Lv / (IFS_tools.cpd * self.exn) * self.ql  # Liquid water potential temperature (K)
-        self.rho  = self.p / (IFS_tools.Rd * self.Tv)  # Density at full levels (kg m-3)
-        self.wls  = -self.w / (self.rho * IFS_tools.grav)  # Vertical velocity (m s-1)
+        self.thl  = self.th - ifs_tools.Lv / (ifs_tools.cpd * self.exn) * self.ql  # Liquid water potential temperature (K)
+        self.rho  = self.p / (ifs_tools.Rd * self.Tv)  # Density at full levels (kg m-3)
+        self.wls  = -self.w / (self.rho * ifs_tools.grav)  # Vertical velocity (m s-1)
         self.U    = (self.u**2. + self.v**2)**0.5  # Absolute horizontal wind (m s-1)
 
-        self.Tvs  = IFS_tools.calc_virtual_temp(self.Ts, self.q[:,0])  # Estimate surface Tv using lowest model q (...)
-        self.rhos = self.ph[:,0] / (IFS_tools.Rd * self.Tvs)  # Surface density (kg m-3)
-        self.exns = IFS_tools.calc_exner(self.ps)  # Exner at surface (-)
-        self.wths = self.H / (self.rhos * IFS_tools.cpd * self.exns)  # Surface kinematic heat flux (K m s-1)
+        self.Tvs  = ifs_tools.calc_virtual_temp(self.Ts, self.q[:,0])  # Estimate surface Tv using lowest model q (...)
+        self.rhos = self.ph[:,0] / (ifs_tools.Rd * self.Tvs)  # Surface density (kg m-3)
+        self.exns = ifs_tools.calc_exner(self.ps)  # Exner at surface (-)
+        self.wths = self.H / (self.rhos * ifs_tools.cpd * self.exns)  # Surface kinematic heat flux (K m s-1)
 
         self.fc = 2 * 7.2921e-5 * np.sin(np.deg2rad(self.settings['central_lat']))  # Coriolis parameter
 
@@ -335,6 +337,11 @@ class Read_era5:
         message('Using nearest lat/lon = {0:.2f}/{1:.2f} (requested = {2:.2f}/{3:.2f}), distance ~= {4:.1f} km'\
                 .format(self.lats[self.j], self.lons[self.i],
                         self.settings['central_lat'], self.settings['central_lon'], distance/1000.))
+
+        # Print averaging area.
+        dlon = (1+2*n_av) * float(self.lons[1] - self.lons[0])
+        dlat = (1+2*n_av) * float(self.lats[1] - self.lats[0])
+        message(f'Averaging ERA5 over a {dlon:.2f}°×{dlat:.2f}° spatial area.')
 
         # Start and end indices of averaging domain:
         istart = self.i - n_av
@@ -425,8 +432,8 @@ class Read_era5:
             dzdx = np.gradient(self.z_p, axis=3) / dxdi[None, None, :, :]
             dzdy = np.gradient(self.z_p, axis=2) / dydj[None, None, :, :]
 
-            self.ug_p = -IFS_tools.grav / self.fc * dzdy
-            self.vg_p =  IFS_tools.grav / self.fc * dzdx
+            self.ug_p = -ifs_tools.grav / self.fc * dzdy
+            self.vg_p =  ifs_tools.grav / self.fc * dzdx
 
             ug_p_mean = self.ug_p[center4d].mean(axis=(2,3))
             vg_p_mean = self.vg_p[center4d].mean(axis=(2,3))
@@ -480,11 +487,11 @@ class Read_era5:
 
             # Geostrophic wind (gradient geopotential height on constant pressure levels)
             vg_p_mean = (
-                IFS_tools.grav / self.fc * fd.grad4c(
+                ifs_tools.grav / self.fc * fd.grad4c(
                     self.z_p[s(0,-2)], self.z_p[s(0,-1)], self.z_p[s(0,+1)], self.z_p[s(0,+2)], dx)
                         ).mean(axis=(2,3))
             ug_p_mean = (
-               -IFS_tools.grav / self.fc * fd.grad4c(
+               -ifs_tools.grav / self.fc * fd.grad4c(
                     self.z_p[s(-2,0)], self.z_p[s(-1,0)], self.z_p[s(+1,0)], self.z_p[s(+2,0)], dy)
                         ).mean(axis=(2,3))
 