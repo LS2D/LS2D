f588ac6b3b06d0221ba4b8ecfc4a5cb2c9e9b1bd
Bart van Stratum <bart@vanstratum.com>
2025-01-07 12:52:59 +0100

    `ds.dims` -> `ds.sizes` to get rid of Xarray warnings.

--- ls2d/ecmwf/read_cams.py
+++ ls2d/ecmwf/read_cams.py
@@ -76,7 +76,7 @@ class Read_cams:
         for date in an_dates:
             for cams_type in ['eac4_ml', 'eac4_sfc', 'egg4_ml']:
                 if cams_type in self.variables.keys():
-                    
+
                     if cams_type == 'egg4_ml':
                         error('Processing CAMS EGG4 data currently does not work due to an open ADS bug.')
 
@@ -119,7 +119,7 @@ class Read_cams:
         # Short-cut
         ds = self.ds_ml
 
-        dims = self.ds_ml.dims
+        dims = self.ds_ml.sizes
         dim_name = ['time', 'level', 'latitude', 'longitude']
 
         ntime = dims['time']
@@ -158,14 +158,14 @@ class Read_cams:
         Interpolate variables required for LES onto model grid,
         and return xarray.Dataset with all possible LES input.
         """
-    
+
         # Find nearest index in dataset.
         clon = self.settings['central_lon']
         clat = self.settings['central_lat']
-    
+
         ic = int(np.abs(self.ds_ml.longitude - clon).argmin())
         jc = int(np.abs(self.ds_ml.latitude  - clat).argmin())
-    
+
         # Some debugging output
         distance = spatial.haversine(self.ds_ml.longitude[ic], self.ds_ml.latitude[jc], clon, clat)
         message('Using nearest lat/lon = {0:.2f}/{1:.2f} (requested = {2:.2f}/{3:.2f}), distance ~= {4:.1f} km'\
@@ -175,7 +175,7 @@ class Read_cams:
         dlon = (1+2*n_av) * abs(float(self.ds_ml.longitude[1] - self.ds_ml.longitude[0]))
         dlat = (1+2*n_av) * abs(float(self.ds_ml.latitude[0] - self.ds_ml.latitude[1]))
         message(f'Averaging CAMS over a {dlon:.2f}°×{dlat:.2f}° spatial area.')
-    
+
         # Slice out averaging sub-domain, and calculate mean over sub-domain.
         self.ds_ml = self.ds_ml.isel(longitude=slice(ic-n_av, ic+n_av+1), latitude=slice(jc-n_av, jc+n_av+1))
 
@@ -187,46 +187,46 @@ class Read_cams:
 
         # Calculate Spatial mean over requested area.
         self.ds_ml_mean = self.ds_ml.mean(dim=['longitude', 'latitude'])
-    
-        # Create new Dataset with LES model levels as main height coordinate.    
+
+        # Create new Dataset with LES model levels as main height coordinate.
         self.ds_les = xr.Dataset(
                 coords = {
                     'time': self.ds_ml.time,
                     'z': z,
                     'lay': self.ds_ml.level.values
                 })
-    
+
         dims_sfc = ['time']
         dims_lay = ['time', 'lay']
         dims_les = ['time', 'z']
-    
-        ntime = self.ds_les.dims['time']
-        ktot = self.ds_les.dims['z']
-    
+
+        ntime = self.ds_les.sizes['time']
+        ktot = self.ds_les.sizes['z']
+
         # Parse all variables in the CAMS NetCDF files. This might be more than the variables
         # specified in settings['cams_vars'], but since those variable names differ from
         # the variable names in NetCDF, it is difficult to link them without a huge lookup
         # table containing **ALL** possible CAMS variables...
         blacklist = ['level', 'time']
-    
+
         for name, da in self.ds_ml_mean.data_vars.items():
             if name not in blacklist and da.ndim == 2:
-    
+
                 # Add "raw" data on CAMS model levels.
                 self.ds_les[f'{name}_lay'] = (dims_lay, da.values)
-    
+
                 if name != 'z':
                     # Interpolate data onto LES grid.
                     out = np.empty((ntime, ktot), np.float32)
-    
+
                     for t in range(ntime):
                         out[t,:] = interpolate.interp1d(
                                 self.ds_ml_mean['z'][t,:].values, da[t,:].values, fill_value='extrapolate')(z)
-    
+
                     self.ds_les[name] = (dims_les, out)
 
         # Calculate time in seconds since start of LES.
         date = self.ds_les.time.values
         self.ds_les['time_sec'] = (date - date[0]).astype(np.float32) * 1e-9
-    
+
         return self.ds_les
